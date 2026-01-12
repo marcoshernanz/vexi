af6f9c0f97c5a2da97be181e332f080bd36c0c1d
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-01-12 11:11:43 +0100

    feat: implement Validator classes for type validation and utility functions

---
+++ sdk/src/fields.ts
@@ -0,0 +1,46 @@
+export abstract class Validator<Type> {
+  readonly type!: Type;
+  readonly isVexiValidator = true;
+
+  constructor(readonly isOptional: boolean = false) {}
+}
+
+export class VBoolean<Type = boolean> extends Validator<Type> {
+  constructor() {
+    super(false);
+  }
+}
+
+export class VNumber<Type = number> extends Validator<Type> {
+  constructor() {
+    super(false);
+  }
+}
+
+export class VString<Type = string> extends Validator<Type> {
+  constructor() {
+    super(false);
+  }
+}
+
+export class VOptional<T extends Validator<any>> extends Validator<
+  T["type"] | undefined
+> {
+  constructor(readonly validator: T) {
+    super(true);
+  }
+}
+
+export const v = {
+  boolean: () => new VBoolean(),
+  number: () => new VNumber(),
+  string: () => new VString(),
+  optional: <T extends Validator<any>>(
+    value: T,
+  ): T extends VOptional<any> ? T : VOptional<T> => {
+    if (value instanceof VOptional) {
+      return value as any;
+    }
+    return new VOptional(value) as any;
+  },
+};
