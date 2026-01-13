ecd5cb0a4307ab44c8b5744045ec438bdf5a4baf
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2026-01-13 20:12:14 +0100

    feat: add LanceDB connection and directory setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,19 @@
 import Fastify from "fastify";
+import * as lancedb from "@lancedb/lancedb";
+import * as fs from "fs";
+import * as path from "path";
 
 const fastify = Fastify({
   logger: true,
 });
+
+const dbDir = path.join(process.cwd(), ".lancedb");
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir);
+}
+
+const db = await lancedb.connect(dbDir);
+fastify.log.info(`Connected to LanceDB at ${dbDir}`);
 
 fastify.get("/", async (request, reply) => {
   return { hello: "world" };
